09685c9f0461e4c0924b16ac33327faa38e1bbde
JinoMatera <jinomatera6@gmail.com>
2025-04-14 13:36:13 +0800

    Add ecommerce/config.php 1744608971220

---
+++ ecommerce/config.php
@@ -0,0 +1,12 @@
+<?php
+// Database configuration (SQLite)
+define('DB_PATH', __DIR__ . '/database/ecommerce.db');
+
+// Site configuration
+define('SITE_NAME', 'My eCommerce');
+define('BASE_URL', 'http://localhost/ecommerce');
+
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
